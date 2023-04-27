bec2005d66b30eb4a53c85aab76b431874b63246
naomieliana7 <131322122+naomieliana7@users.noreply.github.com>
2023-04-26 23:57:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
 let intentos = 6;
-let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
-const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+let palabra = 'APPLE';
+const API = 'https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase';
 
-const button = document.getElementById("guess-button");
-button.addEventListener("click", intentar);
+fetch(API).then(response => response.json())
+.then(response => {
+    console.log(response);
+})
+.catch(err => console.log(err))
+
+const BOTON = document.getElementById("guess-button");
+BOTON.addEventListener("click", intentar);
 console.log(palabra);
 
 
 function intentar(){
     const INTENTO = leerIntento();
 
+    
     if (INTENTO === palabra ) {
-        terminar("<h1>GANASTE!😀</h1>")
-        return
+        terminar("<h1> GANASTE!😀 </h1>")
     }
 
     const GRID = document.getElementById("grid");
     const ROW = document.createElement('div');
     ROW.className = 'row';
 
-    for (let i in palabra){
-        const SPAN = document.createElement('span');
-        SPAN.className = 'letter';
-        if (INTENTO[i]===palabra[i]){ //VERDE
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'green';
-        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'yellow';
-        } else {      //GRIS
-            SPAN.innerHTML = INTENTO[i];
-            SPAN.style.backgroundColor = 'grey';
+   
+        for (let i in palabra){
+            const SPAN = document.createElement('span');
+            SPAN.style.borderRadius='4px';
+            
+            SPAN.className = 'letter';
+            if (INTENTO[i]===palabra[i]){ //VERDE
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = 'green';
+            } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = 'yellow';
+            } else {      //GRIS
+                SPAN.innerHTML = INTENTO[i];
+                SPAN.style.backgroundColor = 'grey';
+            }
+            ROW.appendChild(SPAN)
+            
         }
-        ROW.appendChild(SPAN)
-    }
+    
     GRID.appendChild(ROW)
 
     intentos--;
-    if(intentos==0){
-        terminar("<h1>PERDISTE!😖</h1>")
+    if(intentos===0){
+        terminar("<h1> PERDISTE!😖 </h1>")
     }
 }
 
@@ -55,4 +66,7 @@ function terminar(mensaje){
     BOTON.disabled = true;
     let contenedor = document.getElementById('guesses');
     contenedor.innerHTML = mensaje;
+}
+function advertenciaIngreso(mensaje){
+
 }
